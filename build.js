0e9b3195424555f70cc5bcd6b6fe55662ab7a0d2
drori200 <32238971+drori200@users.noreply.github.com>
2020-12-27 02:51:29 +0200

    Initial

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 const builder = require('electron-builder')
 const Platform = builder.Platform
 
-function getCurrentPlatform(){
-    switch(process.platform){
+function getCurrentPlatform() {
+    switch (process.platform) {
         case 'win32':
             return Platform.WINDOWS
         case 'darwin':
@@ -18,21 +18,19 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
-        artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        appId: 'havencorenetworklauncher',
+        productName: 'HavenCore Network Launcher',
+        artifactName: '${productName} Setup ${version}.${ext}',
+        copyright: 'Copyright © 2018-2020 HavenCore Network',
         directories: {
             buildResources: 'build',
             output: 'dist'
         },
         win: {
-            target: [
-                {
-                    target: 'nsis',
-                    arch: 'x64'
-                }
-            ]
+            target: [{
+                target: 'nsis',
+                arch: 'x64'
+            }]
         },
         nsis: {
             oneClick: false,
@@ -46,10 +44,10 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            maintainer: 'HavenCore Network',
+            vendor: 'HavenCore Network',
+            synopsis: 'The HavenCore Network Modded Minecraft Launcher',
+            description: 'Custom launcher which allows users to join HavenCore\'s modded servers. All mods, configurations, and updates are handled automatically.',
             category: 'Game'
         },
         compression: 'maximum',
